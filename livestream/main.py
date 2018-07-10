bbed74e00026c9005d9f3bf2153bdaad27e9c7a2
Benedict Lau <ben.hy.lau@gmail.com>
2018-07-10 05:10:39 -0400

    Fix file_path check in livestream script

---
+++ livestream/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import datetime
+import obspython as obs
+import yaml
+
+refresh_interval = 15000 # In seconds
+
+def script_description():
+  return ('Conference programming read from a YAML file with schema:\n'
+          '- current_title: "First Title"\n'
+          '  current_presenter: "First Presenter"\n'
+          '  next_title: "Next Title"\n'
+          '  next_presenter: "Next Presenter"\n'
+          '  next_time: 2018-07-01 10:00')
+
+def script_properties():
+  props = obs.obs_properties_create()
+  obs.obs_properties_add_path(props, 'file_path', 'File Path', obs.OBS_PATH_FILE, 'YAML file (*.yml)', 'current.yml')
+  return props
+
+def script_update(settings):
+  global refresh_interval
+
+  obs.timer_remove(refresh_all)
+  file_path = obs.obs_get_source_by_name('file_path')
+  if file_path is not None:
+    obs.timer_add(refresh_all, refresh_interval)
+    obs.obs_source_release(file_path)
+
+def refresh_all():
+  file_path = obs.obs_get_source_by_name('file_path')
+  if file_path is not None:
+    with open(file_path, 'r') as current:
+      try:
+        session_yml = current.read()
+        session = yaml.load(session_yml)[0]
+        refresh_item('current_title', session['current_title'])
+        refresh_item('current_presenter', session['current_presenter'])
+        refresh_item('next_title', session['next_title'])
+        refresh_item('next_presenter', session['next_presenter'])
+        refresh_item('next_time', '{:%b %d %I:%M %p EST}'.format(datetime.datetime.strptime(session['next_time'], '%Y-%m-%d %H:%M')))
+      except yaml.YAMLError as e:
+        print(e)
+    obs.obs_source_release(file_path)
+
+def refresh_item(key, value):
+  settings = obs.obs_data_create()
+  obs.obs_data_set_string(settings, key, value)
+  obs.obs_source_update(source, settings)
+  obs.obs_data_release(settings)
